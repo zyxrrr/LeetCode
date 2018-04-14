df1fc89b6d64ed60bf8404207cf76e01c79a9051
ZYX <seu_zyx@163.com>
2018-04-14 14:38:36 +0800

    Update Solution.cpp

--- 002-Add Two Numbers/Solution.cpp
+++ 002-Add Two Numbers/Solution.cpp
@@ -11,35 +11,48 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int sum=0,carry=0;
         ListNode* pre=NULL;
+        ListNode* res=NULL;
+        if(l1 && l2){
+            sum=l1->val+l2->val+carry;
+            carry=sum/10;
+            sum=sum%10;
+            ListNode* tmp=new ListNode(sum);            
+            pre=res=tmp;
+            l1=l1->next;
+            l2=l2->next;
+        }
         while(l1 && l2){
             sum=l1->val+l2->val+carry;
             carry=sum/10;
             sum=sum%10;
             ListNode* tmp=new ListNode(sum);
-            tmp->next=pre;
+            pre->next=tmp;
             pre=tmp;
+            l1=l1->next;
+            l2=l2->next;
         }
         while(l1){
             sum=l1->val+carry;
             carry=sum/10;
             sum=sum%10;
             ListNode* tmp=new ListNode(sum);
-            tmp->next=pre;
+            pre->next=tmp;
             pre=tmp;
+            l1=l1->next;
         }
         while(l2){
             sum=l2->val+carry;
             carry=sum/10;
             sum=sum%10;
             ListNode* tmp=new ListNode(sum);
-            tmp->next=pre;
+            pre->next=tmp;
             pre=tmp;
+            l2=l2->next;
         }
         if(carry){
             ListNode* tmp=new ListNode(carry);
-            tmp->next=pre;
-            pre=tmp;
+            pre->next=tmp;
         }
-        return pre;
+        return res;
     }
 };
