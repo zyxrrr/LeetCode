db5d21fc41207dca80730a3dadb00a7846d24ec5
ZYX <seu_zyx@163.com>
2018-04-14 19:58:15 +0800

    Update Solution.cpp

--- 003-Longest Substring Without Repeating Characters/Solution.cpp
+++ 003-Longest Substring Without Repeating Characters/Solution.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         vector<int> hash(256,-1);
-        vector<int> tmp(256,-1);
         int len=s.size();
         int sta=0,end=0,max=0,count=0;        
         while(end<len){
             char c=s[end];
-            if(hash[c-0]==-1){
+            if(hash[c-0]<sta){
                 count=end-sta+1;
                 max=count>max?count:max;
                 hash[c-0]=end;
                 end++;
             }else{                
-                sta=hash[c-0]+1;
-                hash=tmp;
-                for(int i=sta;i<=end;i++){
-                    hash[s[i]-0]=i;
-                }
-                count=end-sta+1;
+                sta=hash[c-0]+1;                
+                hash[c-0]=end;
                 end++;
             }            
         }
