70ad12725a866142b5385179e597ceaa48a1e81a
ZYX <seu_zyx@163.com>
2018-08-13 20:39:44 +0800

    Update Solution.cpp

--- 438-Find All Anagrams in a String/Solution.cpp
+++ 438-Find All Anagrams in a String/Solution.cpp
@@ -2,34 +2,40 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         vector<int> res;
-        if (p.empty() || s.empty() || p.size() > s.size()){return res;}
-        int a[128] = {0};
-        int o[129] = {0};
+        map<char, int> cnt;
+        if (s.size() < p.size() || p.empty()){return res;}
+        
         for (int i = 0; i < p.size(); i++){
-            int idx = p[i] - '0';
-            a[idx]++;
-            o[idx]++;
+            char key = p[i];
+            cnt[key]++;
         }
-        for (int i = 0; i < s.size() - p.size() + 1; i++){
-            bool flag = true;
-            for (int j = 0; j < p.size(); j++){
-                int idx = s[i+j] - '0';
-                //cout << s[i+j] << endl;
-                //cout << idx <<endl;
-                if (a[idx] > 0){
-                    a[idx]--;
-                }else{
-                    flag = false;
-                    break;
+        int beg = 0, end = 0, len = s.size(), counter = cnt.size();
+        while (end < len){
+            char tmp = s[end];
+            auto it = cnt.find(tmp);
+            if (it != cnt.end()){
+                it->second--;
+                if (it->second == 0){
+                    counter--;
                 }
             }
-            if (flag){
-                res.push_back(i);
-            }
-            for (int i = 0; i < 128; i++){
+            
+            end++;
+            
+            while(counter == 0){
+                char tmp = s[beg];
+                auto it = cnt.find(tmp);
+                if (it != cnt.end()){
+                    it->second++;
+                    if (it->second > 0){
+                        counter++;
+                    }
+                }
                 
-                a[i] = o[i];
-                
+                if (end - beg == p.size()){
+                    res.push_back(beg);
+                }
+                beg++;
             }
         }
         return res;
