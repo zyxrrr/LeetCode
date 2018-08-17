35a4b798af8ea8e5d5d2a3366bd245384400e35d
ZYX <seu_zyx@163.com>
2018-08-17 14:57:37 +0800

    Create Solution.cpp

---
+++ 617-Merge Two Binary Trees/Solution.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {        
+        if (t1 && t2){
+            TreeNode *r = new TreeNode(t1->val + t2->val);
+            r->left = mergeTrees(t1->left, t2->left);
+            r->right = mergeTrees(t1->right, t2->right);
+            return r;
+        }else if (t1 && !t2){
+            TreeNode *r = new TreeNode(t1->val);
+            r->left = mergeTrees(t1->left, NULL);
+            r->right = mergeTrees(t1->right, NULL);
+            return r;
+        }else if (!t1 && t2){
+            TreeNode *r = new TreeNode(t2->val);
+            r->left = mergeTrees(NULL, t2->left);
+            r->right = mergeTrees(NULL, t2->right);
+            return r;
+        }else{
+            return NULL;
+        }
+    }
+};
